6c0977cb7f66a5007aa891da0e833d07d37417a7
Kokumaji <kokumaji@outlook.com>
2022-04-30 12:05:24 +0200

    Initial Commit :tada:

--- base/src/main/java/dev/kkmji/hibiscus/base/Hibiscus.java
+++ base/src/main/java/dev/kkmji/hibiscus/base/Hibiscus.java
@@ -1,4 +1,5 @@
 package dev.kkmji.hibiscus.base;
 
 public class Hibiscus {
+    
 }
---
+++ bukkit/src/main/java/dev/kkmji/hibiscus/bukkit/HibiscusPlugin.java
@@ -0,0 +1,11 @@
+package dev.kkmji.hibiscus.bukkit;
+
+import dev.kkmji.hibiscus.base.utils.MathUtil;
+
+public class HibiscusPlugin {
+    
+    public static void test() {
+        MathUtil.randomInt(10);
+    }
+
+}
